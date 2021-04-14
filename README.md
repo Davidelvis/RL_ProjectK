0f6399a59c29d836f2a398be46f58ccb9f56b40d
Davidelvis <komboelvis08@gmail.com>
2021-04-14 23:05:14 +0300

    Update on ReadME

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# RL_ProjectK
+# The Anatomy of Simple Reinforcement Learning
+##Reinforcement Learning
+It is where a decision-making system is trained to make optimal decisions
+An agent acting in an environment. At every point in time, 
+the agent observes the state of the environment and decides on an action that changes the state. For each such action, the agent is given a reward signal. The agent’s role is to maximize the total received reward.
+
+**In this RL_basic project,** 
+
+I will define an environment that will give the agent random rewards for a limited number
+of steps, regardless of the agent's actions. 
+It is implemented in baby steps and it is perfect for a newbie in Reinforcement Learning.
+More projects with advanced and efficient techniques in RL are coming!!
+
--- src/env.py
+++ src/env.py
@@ -1,6 +1,5 @@
 import random
-
-import gym
+from typing import List
 
 
 
@@ -9,18 +8,18 @@ class Environment:
     def __init__(self):
         self.steps_left=10
     """Below function returns the observation state of the environment to the Agent"""
-    def get_observation(self) -> List [float]:
+    def get_observation(self) -> List[float]:
         return[0.0,0.0,0.0]
     """Below function is a query of the Agent to the environment on the possible set of actions
     In this case, the possible actions are 0 and 1
     """
-    def get_actions(self) ->List [int]:
+    def get_actions(self) -> List[int]:
         return[0,1]
     """The below code is to signal the  end of an episode.
     A series of interactions between the Agent and the environment are divided into sequences called Episodes
     """
-    def is_done(self) ->bool:
-        return self.steps_left ==0
+    def is_done(self) -> bool:
+        return self.steps_left == 0
     """ Action class(below), is the central piece of the environment's functionality, and it does two things-
     One, It handles the actions from the Agent.
     Two, It returns the rewards from the Environment.
